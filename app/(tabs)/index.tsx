74ea9734bf0bfd39fb94d7b418fe09a51e34bf9b
Bruno0M <brunomedeirosr09@gmail.com>
2025-04-29 17:24:37 -0300

    WIP test notifications

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,85 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import {
+  addQuotation,
+  getQuotations,
+  initDatabase,
+} from "@/data/database.native";
+import { useEffect, useRef, useState } from "react";
+import { Quotation } from "@/data/types";
+import { registerForPushNotificationsAsync } from "@/services/notificationService";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-// import { addQuotation, getQuotations, initDatabase } from '@/data/database';
-import { useEffect, useState } from 'react';
-// import { Quotation } from '@/data/types';
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function HomeScreen() {
-  // const [quotations, setQuotations] = useState<Quotation[]>([]);
-  const [quotations, setQuotations] = useState<any[]>([]);
+  const [quotations, setQuotations] = useState<Quotation[]>([]);
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
 
   useEffect(() => {
-    // const setupDatabase = async () => {
-    //   try {
-    //     console.log('Setting up database...');
-    //     await initDatabase();
-    //     // await handleAddQuotation(); // <-- Mock data insertion (commente this line after insertion)
-    //     await loadQuotations();
-    //   } catch (error) {
-    //     console.error('Error setting up database:', error);
-    //   }
-    // };
-    // setupDatabase();
+    registerForPushNotificationsAsync();
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("Notification received:", notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("Notification response received:", response);
+      });
+
+    const setupDatabase = async () => {
+      try {
+        console.log("Setting up database...");
+        await initDatabase();
+        // await handleAddQuotation(); // <-- Mock data insertion (commente this line after insertion)
+        await loadQuotations();
+      } catch (error) {
+        console.error("Error setting up database:", error);
+      }
+    };
+    setupDatabase();
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
   }, []);
+
+  const sendTestNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Test Notification",
+        body: "This is a test notification",
+        data: { data: "Test data" },
+      },
+      trigger: { type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL, seconds: 2 },
+    });
+  }
 
   const loadQuotations = async () => {
     try {
-      // const fetchedQuotations = await getQuotations() as Quotation[];
-      // setQuotations(fetchedQuotations);
+      const fetchedQuotations = (await getQuotations()) as Quotation[];
+      setQuotations(fetchedQuotations);
     } catch (error) {
-      console.error('Error loading quotations:', error);
+      console.error("Error loading quotations:", error);
     }
   };
 
@@ -41,55 +89,46 @@ export default function HomeScreen() {
       const quotationsList = [
         {
           id: 0,
-          currency: 'USD',
-          latest_date: '2023-10-01',
-          previous_date: '2023-09-30',
+          currency: "USD",
+          latest_date: "2023-10-01",
+          previous_date: "2023-09-30",
           latest_value: 1.0,
           previous_value: 0.95,
         },
         {
           id: 1,
-          currency: 'EUR',
-          latest_date: '2023-10-01',
-          previous_date: '2023-09-30',
+          currency: "EUR",
+          latest_date: "2023-10-01",
+          previous_date: "2023-09-30",
           latest_value: 0.85,
-          previous_value: 0.80,
+          previous_value: 0.8,
         },
         {
           id: 2,
-          currency: 'GBP',
-          latest_date: '2023-10-01',
-          previous_date: '2023-09-30',
+          currency: "GBP",
+          latest_date: "2023-10-01",
+          previous_date: "2023-09-30",
           latest_value: 0.75,
-          previous_value: 0.70,
+          previous_value: 0.7,
         },
       ];
-      
-      // for (const quotation of quotationsList) {
-      //   await addQuotation(quotation);
-      // }
-      // await loadQuotations();
+
+      for (const quotation of quotationsList) {
+        await addQuotation(quotation);
+      }
+      await loadQuotations();
     } catch (error) {
-      console.error('Error adding quotations:', error);
+      console.error("Error adding quotations:", error);
     }
   };
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#353636', dark: '#353636' }}
-      headerImage={<HelloWave />}
-    >
-      <ThemedView style={[styles.container]}>
-        <ThemedView style={[styles.titleContainer]}>
-          <ThemedText type="title">Home</ThemedText>
-        </ThemedView>
-        {quotations.map((quotation) => (
-          <ThemedText key={quotation.id} style={styles.quotationText}>
-            {`${quotation.currency}: ${quotation.latest_value}`}
-          </ThemedText>
-        ))}
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button
+        title="Send Test Notification"
+        onPress={sendTestNotification}
+      />
+    </View>
   );
 }
 
@@ -99,11 +138,11 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   titleContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 16,
   },
   quotationText: {
     marginVertical: 4,
-  }
+  },
 });
